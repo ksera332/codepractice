fa5a7332596422d9fe01e7c789e1fb66c3f8f773
SHOGO501 <ksera6@gmail.com>
2020-08-08 13:16:35 +0900

    言語ごとに整理

--- Eclipse_test.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 2019/02/03
-
-@author: ksera
-'''
-print("Eclipse test")
--- object_01/src/object_01/Main.java
+++
@@ -1,9 +0,0 @@
-package object_01;
-public class Main {
-	public static void main(String[] args) {
-		Judge saito = new Judge();
-		Player murata = new Player("村田");
-		Player yamada = new Player("山田");
-		saito.startJanken(murata, yamada);
-	}
-}
