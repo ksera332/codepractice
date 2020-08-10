10568736413a787e943d3122d9e3bf8877630e60
SHOGO501 <ksera6@gmail.com>
2020-08-10 14:49:02 +0900

    HTML event

--- javascript/test.js
+++ javascript/test.js
@@ -2,6 +2,10 @@ function myFunction(p1,p2){
     return p1 * p2;
 }
 
+function displayDate(){
+    document.getElementById("time").innerHTML = Date();
+}
+
 var person = {
     firstName:"john",
     lastName: "Doe",
