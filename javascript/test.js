d52b3ab979bb821c3c62be55cf7c1f0ab3a441cc
SHOGO501 <ksera6@gmail.com>
2020-08-13 13:37:57 +0900

    loop

--- javascript/test.js
+++ javascript/test.js
@@ -17,3 +17,10 @@ document.getElementById("test").innerHTML = person.fullName();
 
 var txt = "slice test string";
 document.getElementById("slice").innerHTML = txt.slice(3,8);
+
+var cars = ["TOYOTA","HONDA","NISSAN"];
+var text = "";
+for(var i = 0;i<cars.length;i++){
+    text += cars[i] + "<br>";
+}
+document.getElementById("loop").innerHTML = text;
