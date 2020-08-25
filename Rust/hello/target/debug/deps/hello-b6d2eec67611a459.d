bbbcdf72de61e3966aac427dace6c1b17bd59dfa
SHOGO501 <ksera6@gmail.com>
2020-08-25 19:50:37 +0900

    rust-hello-world

---
+++ Rust/hello/target/debug/deps/hello-b6d2eec67611a459.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ksera/Desktop/codepractice/rust/hello/target/debug/deps/hello-b6d2eec67611a459: src/main.rs
+
+/mnt/c/Users/ksera/Desktop/codepractice/rust/hello/target/debug/deps/hello-b6d2eec67611a459.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ksera/Desktop/codepractice/rust/hello/target/debug/hello: /mnt/c/Users/ksera/Desktop/codepractice/rust/hello/src/main.rs
