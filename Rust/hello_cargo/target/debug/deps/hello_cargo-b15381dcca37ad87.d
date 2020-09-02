4fff77e86c209dd4411af9e1450556ed8d2fc613
ksera322 <ksera6@gmail.com>
2020-09-02 21:38:55 +0900

    cargo tutorial

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ksera/Desktop/git/codepractice/Rust/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87: src/main.rs
+
+/mnt/c/Users/ksera/Desktop/git/codepractice/Rust/hello_cargo/target/debug/deps/hello_cargo-b15381dcca37ad87.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ksera/Desktop/git/codepractice/Rust/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/ksera/Desktop/git/codepractice/Rust/hello_cargo/src/main.rs
